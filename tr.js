ee7ff8367f3fb36743f1a103dd55ac4ca9efa5d0
Laboratório Informática <42labinfo@senacrjedu.local>
2022-08-10 18:56:08 -0300

    atividade

---
+++ tr.js
@@ -0,0 +1,16 @@
+function trocaTema(){
+    local = document.getElementById('local').value;
+
+    if (local == 'Centro'){
+        
+    }
+    else if (local == 'Barra da Tijuca'){
+
+    }
+    else if (local == 'Botafogo'){
+
+    }
+
+
+
+}
